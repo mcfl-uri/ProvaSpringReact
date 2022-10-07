28a18ca22c515f8443f7c2b0a01b7c421566e40d
mcfl-uri <lamelasoriol@gmail.com>
2022-10-07 18:03:15 +0200

    Initial commit

---
+++ src/main/java/cat/project/provaspringreact/ProvaSpringReactApplication.java
@@ -0,0 +1,13 @@
+package cat.project.provaspringreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaSpringReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvaSpringReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cat/project/provaspringreact/ProvaSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.project.provaspringreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaSpringReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
